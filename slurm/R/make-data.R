68ba6505dec9fc5922b407c30f736fad12b170d9
rmorsomme <rmorsomme@gmail.com>
2023-12-07 10:04:53 -0500

    record parameters of simulated data

--- slurm/R/make-data.R
+++ slurm/R/make-data.R
@@ -16,34 +16,56 @@ set.seed(1)
 #
 # Model setup ####
 t0 <- 30
-shape_H <- 2
-shape_P <- 1
-
+parameter_from_Ryser2022 = FALSE
 
 #
 # Parameters ####
-# these values are taken from Ryser et al. (2022)
-theta <- list(
-    rate_H = 7.5e-5, shape_H = shape_H,
-    rate_P = 1/7  , shape_P = shape_P,
-    beta   = 38.5/(38.5+5.8), psi = 0.1
-) %>% update_scales()
+if(parameter_from_Ryser2022) {
+    
+    # these values are taken from Ryser et al. (2022)
+    theta <- list(
+        rate_H = 7.5e-5, shape_H = 2,
+        rate_P = 1/7  , shape_P = 1,
+        beta   = 38.5/(38.5+5.8), psi = 0.1
+    ) %>% update_scales()
+    
+} else {
+    
+    shape_H <- 2
+    shape_P <- 2
+    
+    beta = 0.8
+    psi  = 0.5
+    
+    mean_H <- 100 # expected number of years
+    rate_H <- (mean_H/gamma(1+1/shape_H))^(-shape_H) # this gives a Weibull with mean mean_H_0
+    mean_P <- 10 # expected number of years
+    rate_P <- (mean_P/gamma(1+1/shape_P))^(-shape_P) # this gives a Weibull with mean mean_H_0
+    
+    theta <- list(
+        rate_H = rate_H, shape_H = shape_H,
+        rate_P = rate_P, shape_P = shape_P,
+        beta   = beta  , psi = psi
+    ) %>% update_scales()
+}
 
 
 #
 ## Generate data ####
-n  <- 37e3 #37e3  # sample size
+n  <- 5e4 #37e3  # sample size
 source("R/simulator.R")
 
 
 #
 # Save data ####
-path_data  <- "output/MCMC/simulation/data"
-id_data     <- paste0(
+path_data <- "data/simulated"
+id_data   <- paste0(
     "n=", n,
     "-shape_H=", shape_H,
     "-shape_P=", shape_P,
     "-t0=", t0,
+    "-psi=", psi,
+    "-mean_P=", mean_P,
     ".RDATA"
 )
 
